fdfc2db6cb688352ffef330476f91171820464f5
zakiPoteto <yamas.jto@gmail.com>
2026-05-03 02:33:56 +0900

    feat: タスク詳細ストア

---
+++ lib/features/task_detail/store.dart
@@ -0,0 +1,31 @@
+import 'package:clean_house_app/features/task_detail/action.dart';
+import 'package:clean_house_app/features/task_detail/effects.dart';
+import 'package:clean_house_app/features/task_detail/reducer.dart';
+import 'package:clean_house_app/features/task_detail/state.dart';
+import 'package:clean_house_app/models/task.dart';
+import 'package:clean_house_app/providers/repository_provider.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+typedef TaskDetailStore = ({
+  TaskDetailState state,
+  void Function(TaskDetailAction) dispatch,
+});
+
+TaskDetailStore useTaskDetailStore(WidgetRef ref, Task initialTask) {
+  final repo = ref.watch(taskRepositoryProvider);
+
+  final store = useReducer<TaskDetailState, TaskDetailAction>(
+    reduce,
+    initialState: TaskDetailState(task: initialTask),
+    initialAction: const _NoOp(),
+  );
+
+  useTaskDetailEffects(store.state, store.dispatch, repo);
+
+  return (state: store.state, dispatch: store.dispatch);
+}
+
+final class _NoOp extends TaskDetailAction {
+  const _NoOp();
+}
