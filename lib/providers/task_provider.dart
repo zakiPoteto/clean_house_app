9a3335fdbbec6856fe03aa40ea0ec7efbb43cd24
zakiPoteto <yamas.jto@gmail.com>
2026-05-09 22:43:13 +0900

    fix: exportToFileメソッドの戻り値の型を修正

--- lib/providers/task_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:clean_house_app/models/task.dart';
-import 'package:clean_house_app/providers/repository_provider.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final taskListProvider = FutureProvider<List<Task>>((ref) async {
-  final repo = ref.watch(taskRepositoryProvider);
-  return repo.fetchAll();
-});
